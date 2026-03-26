668dd26824e53bda5b122b4cf2762b8227e4a4ae
Omnia Tawfek <omniatawfek@std.mans.edu.eg>
2026-03-26 19:22:14 +0200

    fetching and updating data without navigation

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import CreateOrder, {
 } from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
 import AppLayout from './ui/AppLayout';
-
+import { action as UpdateOrderAction } from './features/order/UpdateOrder';
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
@@ -39,6 +39,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: UpdateOrderAction,
       },
     ],
   },
